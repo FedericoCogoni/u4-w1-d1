982c91232659aae2cba19131745d6965f4eb4cab
Federico Cogoni <federicocogoni90@gmail.com>
2024-05-20 16:47:10 +0200

    add user inputs

--- src/Es3.java
+++ src/Es3.java
@@ -1,13 +1,29 @@
+import java.util.Scanner;
+
 public class Es3 {
     public static void main(String[] args) {
-        System.out.println(" - Perimetro rettangolo : " + perimRett(10, 10) );
-        System.out.println(" - 10 è pari? : "+ èParioDispari(10)); // Output: 0
-        System.out.println(" - Area triangolo : " + areaTri(30, 30, 30));
+        System.out.print("Inserisci la base: ");
+        Scanner scanner = new Scanner(System.in);
+        double b = Double.parseDouble(scanner.nextLine());
+        System.out.print("Inserisci l'altezza: ");
+        double h = Double.parseDouble(scanner.nextLine());
+        System.out.println(" - Perimetro rettangolo : " + perimRett(b, h) );
+        System.out.print("Inserisci il numero: ");
+        int number = Integer.parseInt(scanner.nextLine());
+        System.out.println(" - 0 = pari, 1 = dispari : "+ ParioDispari(number)); // Output: 0
+        System.out.print("Inserisci il lato 1: ");
+        double l1 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Inserisci il lato 2: ");
+        double l2 = Double.parseDouble(scanner.nextLine());
+        System.out.print("Inserisci il lato 3: ");
+        double l3 = Double.parseDouble(scanner.nextLine());
+        System.out.println(" - Area triangolo : " + areaTri(l1, l2, l3));
+        scanner.close();
     }
     public static double perimRett(double b, double h) {
         return (b + h) * 2;
     }
-    public static int èParioDispari(int number) {
+    public static int ParioDispari(int number) {
         return number % 2 == 0 ? 0 : 1;
     }
     public static double areaTri(double l1, double l2, double l3){
